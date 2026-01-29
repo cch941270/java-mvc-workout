9094c6382392f9c3febb680f0cd8d5a9dbf1613e
cch94 <cch941270@myyahoo.com>
2026-01-29 20:41:24 +0800

    Change leg exercise from record to class

--- src/main/java/com/example/workout/legexercise/LegExercise.java
+++ src/main/java/com/example/workout/legexercise/LegExercise.java
@@ -2,18 +2,62 @@ package com.example.workout.legexercise;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Version;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Positive;
 
-public record LegExercise(
+@Entity
+@Table(name = "leg_exercise")
+public class LegExercise {
     @Id
-    Integer id,
-    LegExerciseType legExerciseType,
-    LocalDateTime startedOn,
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "leg_exercise_seq")
+    @SequenceGenerator(name = "leg_exercise_seq", sequenceName = "leg_exercise_seq", allocationSize = 1)
+    private Integer id;
+    private LegExerciseType legExerciseType;
+    private LocalDateTime startedOn;
     @Positive
-    Integer count,
-    @Version
-    Integer version
-) {}
+    private Integer count;
+
+    public LegExercise() {
+        super();
+    }
+
+    public LegExercise(LegExerciseType legExerciseType, LocalDateTime startedOn, Integer count) {
+        super();
+        this.legExerciseType = legExerciseType;
+        this.startedOn = startedOn;
+        this.count = count;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public LegExerciseType getLegExerciseType() {
+        return legExerciseType;
+    }
+
+    public void setLegExerciseType(LegExerciseType legExerciseType) {
+        this.legExerciseType = legExerciseType;
+    }
+
+    public LocalDateTime getStartedOn() {
+        return startedOn;
+    }
+
+    public void setStartedOn(LocalDateTime startedOn) {
+        this.startedOn = startedOn;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
