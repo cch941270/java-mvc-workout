210c9805961172e732be379fef50b72fc515dff1
cch94 <cch941270@myyahoo.com>
2026-01-29 20:39:21 +0800

    Remove version from leg exercise

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS leg_exercise (
     leg_exercise_type varchar(250) NOT NULL,
     started_on timestamp NOT NULL,
     count INT NOT NULL,
-    version INT NOT NULL,
     PRIMARY KEY (id)
 );
