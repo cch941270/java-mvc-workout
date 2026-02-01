9370336e890ce10eda33822fcd53dae1ec4b64ab
cch94 <cch941270@myyahoo.com>
2026-02-01 22:18:22 +0800

    Disable json data loader

--- src/main/java/com/example/workout/legexercise/LegExerciseJsonDataLoader.java
+++ src/main/java/com/example/workout/legexercise/LegExerciseJsonDataLoader.java
@@ -7,11 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.asm.TypeReference;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Component
+// @Component
 public class LegExerciseJsonDataLoader implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(LegExerciseJsonDataLoader.class);
