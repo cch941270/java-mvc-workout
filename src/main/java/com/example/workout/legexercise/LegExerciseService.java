c1c2c1cc41b831c77f4233e98f724bccc30c012f
cch94 <cch941270@myyahoo.com>
2026-02-02 15:49:57 +0800

    Add leg exercise service

---
+++ src/main/java/com/example/workout/legexercise/LegExerciseService.java
@@ -0,0 +1,32 @@
+package com.example.workout.legexercise;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class LegExerciseService {
+    private final LegExerciseRepository repository;
+
+    public List<LegExerciseDto> findAll() {
+        return repository.findAll().stream().map(this :: convertToDto).toList();
+    }
+
+    public Optional<LegExerciseDto> findById(Integer id) {
+        return repository.findById(id).map(this :: convertToDto);
+    }
+
+    private LegExerciseDto convertToDto(LegExercise legExercise) {
+        return new LegExerciseDto(
+            legExercise.getId(),
+            legExercise.getLegExerciseType(),
+            legExercise.getStartedOn(),
+            legExercise.getCount(),
+            legExercise.getWorkoutUser().getEmail()
+        );
+    }
+}
