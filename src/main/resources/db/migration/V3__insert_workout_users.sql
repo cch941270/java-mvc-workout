3517dfc0ed918419fdacfca619d9a7d26a3819df
cch94 <cch941270@myyahoo.com>
2026-02-04 22:56:52 +0800

    Insert hashed password instead of plain password

--- src/main/resources/db/migration/V3__insert_workout_users.sql
+++ src/main/resources/db/migration/V3__insert_workout_users.sql
@@ -1,4 +1,4 @@
 INSERT INTO workout_users (id, email, hashed_password)
 VALUES
-    (nextval('workout_users_id_seq'), 'user1@example.com', 'password1'),
-    (nextval('workout_users_id_seq'), 'user2@example.com', 'password2');
+    (nextval('workout_users_id_seq'), 'user1@example.com', '$2a$12$OIES19OQ9AuwYKNF5tckneMCtkOuSLm2U84uZ2RlJOY1rPvB6m2DS'),
+    (nextval('workout_users_id_seq'), 'user2@example.com', '$2a$12$oPirXmFPZuEUyf.FBsgivOzcPSvV5CwB9rvDZ89v9EgLsQf0/C6WW');
