432617e05137f76486742205183a170b35217e07
cch94 <cch941270@myyahoo.com>
2026-02-05 02:28:53 +0800

    Update leg exercise controller and service for auth

--- src/main/java/com/example/workout/legexercise/LegExerciseService.java
+++ src/main/java/com/example/workout/legexercise/LegExerciseService.java
@@ -1,10 +1,14 @@
 package com.example.workout.legexercise;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import com.example.workout.workoutuser.WorkoutUser;
+import com.example.workout.workoutuser.WorkoutUserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,8 +16,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LegExerciseService {
     private final LegExerciseRepository repository;
+    private final WorkoutUserRepository workoutUserRepository;
 
-    public List<LegExerciseDto> findAll() {
+    List<LegExerciseDto> findAll() {
         return repository.findAll().stream().map(this :: convertToDto).toList();
     }
 
@@ -22,8 +27,33 @@ public class LegExerciseService {
         return repository.findAll(sortByStartedOnDesc);
     }
 
-    public Optional<LegExerciseDto> findById(Integer id) {
+    List<LegExercise> findAllByUsernameSorted(String username) {
+        List<LegExercise> legExercises = workoutUserRepository.findByUsername(username).get().getLegExercises();
+        legExercises.sort(Comparator.comparing(LegExercise::getStartedOn).reversed());
+        return legExercises;
+    }
+
+    Optional<LegExerciseDto> findById(Integer id) {
         return repository.findById(id).map(this :: convertToDto);
+    }
+
+    Optional<LegExercise> findByUsernameAndId(String username, Integer id) {
+        WorkoutUser workoutUser = workoutUserRepository.findByUsername(username).get();
+        Optional<LegExercise> legExercise = workoutUser.getLegExercises().stream().filter(e -> e.getId() == id).findFirst();
+        return legExercise;
+    }
+
+    void create(String username, LegExercise legExercise) {
+        WorkoutUser workoutUser = workoutUserRepository.findByUsername(username).get();
+        legExercise.setWorkoutUser(workoutUser);
+        repository.save(legExercise);
+    }
+
+    void update(LegExercise legExercise, LegExercise updatedLegExercise) {
+        legExercise.setLegExerciseType(updatedLegExercise.getLegExerciseType());
+        legExercise.setStartedOn(updatedLegExercise.getStartedOn());
+        legExercise.setCount(updatedLegExercise.getCount());
+        repository.save(legExercise);
     }
 
     public LegExerciseDto convertToDto(LegExercise legExercise) {
