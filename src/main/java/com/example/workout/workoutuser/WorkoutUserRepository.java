e359972718413ff09ddfcc8a108702e5f6c25bc8
cch94 <cch941270@myyahoo.com>
2026-02-04 23:00:36 +0800

    Implement findByUsername in workout user repository

--- src/main/java/com/example/workout/workoutuser/WorkoutUserRepository.java
+++ src/main/java/com/example/workout/workoutuser/WorkoutUserRepository.java
@@ -1,8 +1,11 @@
 package com.example.workout.workoutuser;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WorkoutUserRepository extends JpaRepository<WorkoutUser, Long> {
+    Optional<WorkoutUser> findByUsername(String username);
 }
