241091064384a512b51bffd17bb512d8e2dadfb8
cch94 <cch941270@myyahoo.com>
2026-02-05 00:06:18 +0800

    Fix header dropdown menu

--- src/main/java/com/example/workout/SecurityConfig.java
+++ src/main/java/com/example/workout/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.example.workout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -19,8 +21,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(requests -> requests
-                // .requestMatchers("/api/leg-exercise").permitAll()
-                .anyRequest().permitAll()
+                .requestMatchers("/api/**").permitAll()
+                .requestMatchers("/", "/leg-exercises").permitAll()
+                .requestMatchers("/users/new").permitAll()
+                .anyRequest().authenticated()
             )
             .formLogin(form -> form
                 .permitAll()
