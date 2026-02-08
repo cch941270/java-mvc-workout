64c5c404c572bd904a412aa9ca66e74b03369398
cch94 <cch941270@myyahoo.com>
2026-02-08 21:47:47 +0800

    Log error

---
+++ src/main/java/com/example/workout/MyErrorController.java
@@ -0,0 +1,27 @@
+package com.example.workout;
+
+import java.time.Instant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+	private static final Logger logger = LoggerFactory.getLogger(WorkoutApplication.class);
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object originalUri = request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+        logger.error("{} {} {}", Instant.now().toString(), statusCode, originalUri);
+
+        return "error";
+    }
+}
