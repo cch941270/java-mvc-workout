93216dd4e6ed1ce6dd00fdbea6811ac674a4acda
cch94 <cch941270@myyahoo.com>
2026-02-09 14:31:28 +0800

    Show more detail in error page

--- src/main/java/com/example/workout/MyErrorController.java
+++ src/main/java/com/example/workout/MyErrorController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.RequestDispatcher;
@@ -17,11 +18,13 @@ public class MyErrorController implements ErrorController {
 	private static final Logger logger = LoggerFactory.getLogger(WorkoutApplication.class);
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object errorMessage = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
         Object originalUri = request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
-        logger.error("{} {} {}", Instant.now().toString(), statusCode, originalUri);
-
+        logger.error("Datetime: {}, Uri: {}, Status: {}, Message: {}", Instant.now().toString(), originalUri, statusCode, errorMessage);
+        model.addAttribute("statusCode", statusCode);
+        model.addAttribute("errorMessage", errorMessage);
         return "error";
     }
 }
