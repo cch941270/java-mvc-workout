1f3d5512e763b6f11b5e3df15a52423bec860f5a
cch94 <cch941270@myyahoo.com>
2026-02-12 19:36:13 +0800

    Fix N+1 query

--- src/main/java/com/example/workout/legexercise/LegExerciseRepository.java
+++ src/main/java/com/example/workout/legexercise/LegExerciseRepository.java
@@ -2,10 +2,17 @@ package com.example.workout.legexercise;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LegExerciseRepository extends JpaRepository<LegExercise, Integer> {
+
     Optional<LegExercise> findByIdAndWorkoutUserId(Integer id, Long workoutUserId);
+
+    @EntityGraph(attributePaths = {"workoutUser"})
+    Page<LegExercise> findAll(Pageable pageable);
 }
